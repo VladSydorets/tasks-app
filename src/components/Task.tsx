7b925ad421b1596fb25560f9129c72d068496e3e
Vlad <sidorecvv@gmail.com>
2022-11-14 14:50:03 +0100

    add drag and drop

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,55 +1,17 @@
-// import React, { useState, useRef, useEffect } from "react";
 import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
 type TaskProps = {
   id: number;
   text: string;
   completed: boolean;
   setTasks: any;
-  checkTask: any;
-  removeTask: any;
+  index: number;
+  checkTask: (id: number) => void;
+  removeTask: (id: number) => void;
 };
 
 export const Task = (props: TaskProps) => {
-  // function completeAll(todos: readonly Todo[]): CompletedTodo[] {
-  //   return todos.map((todo) => ({
-  //     ...todo,
-  //     done: true,
-  //   }));
-  // }
-
-  // function toggleTodo(todo: Todo): Todo {
-  //   return {
-  //     ...todo,
-  //     done: !todo.done,
-  //   };
-  // }
-
-  // function tagToString(tag: Tag): string {
-  //   return `fa-${tag} ${tag}`;
-  // }
-
-  // const inputRef = useRef<HTMLInputElement>(null);
-
-  // function handleAddTask() {
-  //   // let newArr = [...tasks];
-  //   if (inputRef.current != null && inputRef.current.value !== "") {
-  //     let newTask = {
-  //       id: tasks[tasks.length - 1].id + 1,
-  //       text: `${inputRef.current.value[0].toUpperCase()}${inputRef.current.value.slice(
-  //         1
-  //       )}`,
-  //       done: false,
-  //     };
-  //     setTasks([...tasks, newTask]);
-  //     inputRef.current.value = "";
-  //   }
-  // }
-
-  // function handleInputChange(e: any) {
-  //   if (e.target.value !== "") e.target.value[0].toUpperCase();
-  // }
-
   function handleCheckClick() {
     props.checkTask(props.id);
   }
@@ -62,29 +24,40 @@ export const Task = (props: TaskProps) => {
     "text-pink-400 text-base hover:text-pink-300 hover:cursor-pointer transition-colors ease-in delay-300 duration-300 mr-2";
 
   return (
-    <li
+    <Draggable
       key={props.id}
-      className="py-1.5 px-2 rounded-lg w-full bg-zinc-800 hover:bg-zinc-700 transition-colors ease-in duration-300 hover:cursor-pointer flex justify-between"
+      draggableId={props.id.toString()}
+      index={props.index}
     >
-      <div className="flex">
-        <i
-          className={`fa-regular fa-${
-            props.completed ? "square-check" : "square"
-          } ${boxStyling}`}
-          onClick={handleCheckClick}
-        ></i>
-        <p
-          className={`text-gray-300 font-normal text-base leading-5 align-middle text-left ${
-            props.completed ? "line-through opacity-75" : ""
-          }`}
+      {(provided) => (
+        <li
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          key={props.id}
+          className="py-1.5 px-2 rounded-lg w-full bg-zinc-800 hover:bg-zinc-700 transition-colors ease-in duration-300 hover:cursor-pointer flex justify-between mr-10"
         >
-          {props.text}
-        </p>
-      </div>
-      <i
-        className={`fa-solid fa-xmark justify-self-end ${boxStyling}`}
-        onClick={handleRemoveClick}
-      ></i>
-    </li>
+          <div className="flex">
+            <i
+              className={`fa-regular fa-${
+                props.completed ? "square-check" : "square"
+              } ${boxStyling}`}
+              onClick={handleCheckClick}
+            ></i>
+            <p
+              className={`text-gray-300 font-normal text-base leading-5 align-middle text-left ${
+                props.completed ? "line-through opacity-75" : ""
+              }`}
+            >
+              {props.text}
+            </p>
+          </div>
+          <i
+            className={`fa-solid fa-xmark justify-self-end ${boxStyling}`}
+            onClick={handleRemoveClick}
+          ></i>
+        </li>
+      )}
+    </Draggable>
   );
 };
---
+++ src/components/Todo.tsx
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import { Task } from "./Task";
+import { TaskInput } from "./TaskInput";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+export const Todo = () => {
+  type Tag = "home" | "work" | { custom: string };
+
+  // Readonly Todo task type
+  type Todo = Readonly<{
+    id: number;
+    text: string;
+    done: boolean;
+    tag?: Tag;
+  }>;
+
+  type CompletedTodo = Todo & {
+    readonly done: true;
+  };
+
+  type UnCompletedTodo = Todo & {
+    readonly done: false;
+  };
+
+  // get data from the localStorage and set it tasks state
+  const savedData = JSON.parse(localStorage.getItem("data") || "");
+  const [tasks, setTasks] = useState<Todo[]>(savedData);
+
+  const tasksList = tasks.map((task, index) => (
+    <Task
+      id={task.id}
+      key={task.id}
+      text={task.text}
+      completed={task.done}
+      setTasks={setTasks}
+      index={index}
+      checkTask={checkTask}
+      removeTask={removeTask}
+    />
+  ));
+
+  function addTask(value: string): void {
+    const newTask = {
+      id: tasks.length !== 0 ? tasks[tasks.length - 1].id + 1 : 0,
+      text: value,
+      done: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
+  // toggleTodo function can return only completed or uncompleted task
+  function toggleTodo(todo: Todo): CompletedTodo | UnCompletedTodo {
+    return {
+      ...todo,
+      done: !todo.done,
+    };
+  }
+
+  function checkTask(id: number): void {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? toggleTodo(task) : task
+    );
+    setTasks(updatedTasks);
+  }
+
+  function removeTask(id: number): void {
+    const remainingTasks = tasks.filter((task) => task.id !== id);
+    setTasks(remainingTasks);
+  }
+
+  function completeAll(todos: readonly Todo[]): CompletedTodo[] {
+    return todos.map((todo) => ({
+      ...todo,
+      done: true,
+    }));
+  }
+
+  function handleCheckAll(): void {
+    setTasks(completeAll(tasks));
+  }
+
+  function removeChecked(todos: readonly Todo[]) {
+    return todos.filter((todo) => !todo.done);
+  }
+
+  function handleRemoveChecked(): void {
+    setTasks(removeChecked(tasks));
+  }
+
+  function handleOnDragEnd(result: any): void {
+    if (!result.destination) return;
+
+    const updatedTasks = [...tasks];
+    const [movedTask] = updatedTasks.splice(result.source.index, 1);
+    updatedTasks.splice(result.destination.index, 0, movedTask);
+
+    setTasks(updatedTasks);
+  }
+
+  // save data in the localStorage
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <div className="wrapper h-screen flex justify-center items-center">
+      <div className="container flex flex-col gap-4 w-[30rem] h-5/6 overflow-auto">
+        <TaskInput addTask={addTask} />
+        <h2 className="text-gray-300 text-base font-medium self-start justify-self-start m-3">
+          Tasks - {tasks.length}
+        </h2>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId={"tasks"}>
+            {(provided) => (
+              <ul
+                className="flex flex-col gap-2"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {tasksList}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+        <div className="bottom-btns flex justify-between">
+          <button className="text-gray-300 w-fit" onClick={handleCheckAll}>
+            Mark all as completed
+          </button>
+          <button className="text-gray-300 w-fit" onClick={handleRemoveChecked}>
+            Remove all checked
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
